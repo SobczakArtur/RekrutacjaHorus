fd2d5d1577f46c85e6561acb94e54c463334d54b
Artur Sobczak <sobczak.artur88@gmail.com>
2025-09-30 01:54:55 +0200

    Completing the recruitment task

---
+++ src/FileCabinet.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class FileCabinet implements Cabinet {
+    private List<Folder> folders;
+    private MultiFolder multiFolder;
+
+
+    @Override
+    public Optional<Folder> findFolderByName(String name) {
+        Folder retriveFolderByName = null;
+
+        for (Folder folder : folders) {
+            if (folder.getName() == name){
+                retriveFolderByName = folder;
+            }
+        }
+        return Optional.ofNullable(retriveFolderByName);
+    }
+
+
+    @Override
+    public List<Folder> findFoldersBySize(String size) {
+
+        folders.stream()
+                .filter(folder -> folder.getSize() == size)
+                .collect(Collectors.toList());
+
+        return folders;
+    }
+
+    @Override
+    public int count() {
+        return multiFolder.getFolders().size();
+    }
+}
